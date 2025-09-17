f122b02956ef293f447235de3f8a33268ad99f74
Rodion Altshuler <rodion.altshuler@gmail.com>
2025-09-17 11:03:52 +0200

    Add staleness of arbitrage opportunity to the output

--- src/main.rs
+++ src/main.rs
@@ -57,27 +57,43 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn check_arbitrage(first_exchange: &Exchange,
-                   first: &QuoteUpdate,
-                   second_exchange: &Exchange,
-                   second: &QuoteUpdate) {
+fn check_arbitrage(
+    first_exchange: &Exchange,
+    first: &QuoteUpdate,
+    second_exchange: &Exchange,
+    second: &QuoteUpdate,
+) {
     let diff_buy_first_sell_second = second.best_quote.bid_price - first.best_quote.ask_price;
     let diff_buy_second_sell_first = first.best_quote.bid_price - second.best_quote.ask_price;
 
+    // Opportunity timestamp = older of the two quotes
+    let opp_ts = first.ts.min(second.ts);
+    let now_ms = chrono::Utc::now().timestamp_millis();
+    let staleness = now_ms - opp_ts;
+
     if diff_buy_first_sell_second > 0.1 {
-        println!("Arb: buy {} @ {:.4}, sell {} @ {:.4}, spread {:.4}",
+        println!(
+            "[Arb] ts={} staleness={}ms | Buy {} @ {:.4}, Sell {} @ {:.4}, Spread {:.4}",
+            opp_ts,
+            staleness,
             first_exchange.name,
             first.best_quote.ask_price,
             second_exchange.name,
             second.best_quote.bid_price,
-            diff_buy_first_sell_second);
+            diff_buy_first_sell_second
+        );
     }
+
     if diff_buy_second_sell_first > 0.1 {
-        println!("Arb: buy {} @ {:.4}, sell {} @ {:.4}, spread {:.4}",
-                 second_exchange.name,
-                 second.best_quote.ask_price,
-                 first_exchange.name,
-                 first.best_quote.bid_price,
-                 diff_buy_second_sell_first);
+        println!(
+            "[Arb] ts={} staleness={}ms | Buy {} @ {:.4}, Sell {} @ {:.4}, Spread {:.4}",
+            opp_ts,
+            staleness,
+            second_exchange.name,
+            second.best_quote.ask_price,
+            first_exchange.name,
+            first.best_quote.bid_price,
+            diff_buy_second_sell_first
+        );
     }
 }
